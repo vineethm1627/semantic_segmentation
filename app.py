3eec42909f69a05e8b0138e611bc882c8c7b4afb
vineethm1627 <vineethm1627@gmail.com>
2020-11-19 17:14:50 +0530

    created separate file for model loading

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pixellib
 from pixellib.semantic import semantic_segmentation
+from prediction import *
 
-DATA_URL = "deeplabv3_xception_tf_dim_ordering_tf_kernels.h5"
+#DATA_URL = "deeplabv3_xception_tf_dim_ordering_tf_kernels.h5"
 st.title("Sematic Image Segmentation")
 st.sidebar.title("Semantic Image Segmentation")
 st.sidebar.markdown("deeplabv3+ trained on pascal voc dataset for Semantic Image Segmentation using PixelLib. Here, Different instances of the same object are segmented with the same color map.")
@@ -16,19 +17,21 @@ if img_file is not None:
     img = plt.imread(img_file, 0)
     st.image(img, caption = "Input Image", use_column_width = True)
     
-    # instantiating the semantic segmentation class
-    segment_image = semantic_segmentation()
+    # # instantiating the semantic segmentation class
+    # segment_image = semantic_segmentation()
 
-    # loading the model deeplabv3+ trained on pascal voc dataset.
-    segment_image.load_pascalvoc_model(DATA_URL)
+    # # loading the model deeplabv3+ trained on pascal voc dataset.
+    # segment_image.load_pascalvoc_model(DATA_URL)
 
-    # performing the segmentation on the input image
-    segment_image.segmentAsPascalvoc(img_file, output_image_name = "output_images/out.jpg")
-    out = plt.imread("output_images/out.jpg", 0)
+    # # performing the segmentation on the input image
+    # segment_image.segmentAsPascalvoc(img_file, output_image_name = "output_images/out.jpg")
+    # out = plt.imread("output_images/out.jpg", 0)
 
-    # performing the segmentation on the input image with overlay
-    segment_image.segmentAsPascalvoc(img_file, output_image_name = "output_images/out_overlay.jpg", overlay = True)
-    out_overlay = plt.imread("output_images/out_overlay.jpg")
+    # # performing the segmentation on the input image with overlay
+    # segment_image.segmentAsPascalvoc(img_file, output_image_name = "output_images/out_overlay.jpg", overlay = True)
+    # out_overlay = plt.imread("output_images/out_overlay.jpg")
+
+    out, out_overlay = prediction(img_file)
     
     col1, col2 = st.beta_columns(2)
     col1.image(out, caption = "Segmented Image", use_column_width = True)
